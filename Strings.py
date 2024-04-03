12da289af7e3cf95a8a2b14558b8c14743ad52ca
SyedMahdi1 <131530472+SyedMahdi1@users.noreply.github.com>
2024-04-03 05:01:36 -0700

    Enjoy the codes
    New lesson. Strings. :)

---
+++ Strings.py
@@ -0,0 +1,49 @@
+Number1 = input("Enter Your Number:")
+Number2 = Number1*5
+print(Number2)# If we do this it will write your number 5 times
+# If We Do This
+# then this happen
+Number3 = int(input("Enter Your Number"))
+Number4 = Number3 * 5
+# It will multiply your number
+print(Number4)
+# Another cool thing
+
+num1 = int(input("Enter Your Number"))
+Number5 = num1*10
+print(f'The Product Of {num1} and 5 is {Number5}')
+
+a = int(input("Enter Your Number"))
+b = a - 4
+print(f'When we Subtract from {a} the 4 the answer is {b} ')
+
+c = input("Enter Your Name: ")
+d = input("Input Your Age: ")
+e = input("Enter Your Class Name: ")
+f = int(input('Input Your Marks: '))
+g = (f / 5)
+print(f'Your Name Is {c}. Your Age Is {d}. You Read In Class {e}. Your Percentage of Marks Is {f}%')
+
+
+h = int(input("Enter a number"))
+i = h**3  # This Is A cube
+print(i)
+# For Multiplication we use single star means this '*'
+
+# To Find SquareRoot we do this
+j = int(input("Enter Your Number"))
+k = j**0.5
+print(f'this square root of {j} is {k}')
+
+# But it is Showing me in float now lets turn into integer
+n = int(input("Enter Your Number"))
+m = n**0.5
+print(f'the square root of {n} is {m:.0f} ')
+
+#Using this method it will only show u one zero but it will clear because 0 cannot be determined in front of python or pycharm
+
+# now let do a 49simple way to find a square root
+import math
+o = int(input("Enter Your Number: "))
+p = math.sqrt(o)
+print(f'The square root of {o} is {p:.0f}')
